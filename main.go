83149e638469b057368655668c642c9ff8cca3fc
Bruno Mendes <bruno.mendes@delled.com>
2024-09-20 11:05:02 -0300

    test create user

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/bmendescrypex/CRUD_g/src/database"
+	"github.com/bmendescrypex/CRUD_g/src/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,14 +13,29 @@ func main() {
 
 	router := gin.Default()
 
-	// Rota raiz
+	router.POST("/users", func(c *gin.Context) {
+		var user models.User
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		result := database.DB.Create(&user)
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+	})
+
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Bem-vindo à API",
 		})
 	})
 
-	// Rota de teste
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
